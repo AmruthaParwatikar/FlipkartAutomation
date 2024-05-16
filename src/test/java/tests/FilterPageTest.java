a4f3ff5b9056227a296f7a18b429bfd3cdd6580e
AmruthaParwatikar <93421756+AmruthaParwatikar@users.noreply.github.com>
2024-05-16 13:02:07 +0530

    FilterPageTest.java

--- src/test/java/tests/FilterPageTest.java
+++ src/test/java/tests/FilterPageTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,7 +15,10 @@ public class FilterPageTest {
     @BeforeClass
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "chromedriver/chromedriver-win64/chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        //options.setHeadless(true);
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.flipkart.com");
     }
