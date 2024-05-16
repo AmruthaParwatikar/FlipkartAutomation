ee7f54e090e6d5f9aa03c1eea274f04a82d29ada
AmruthaParwatikar <93421756+AmruthaParwatikar@users.noreply.github.com>
2024-05-16 15:20:22 +0530

    GroceryTestPage.java

--- src/test/java/tests/GroceryTestPage.java
+++ src/test/java/tests/GroceryTestPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -16,7 +17,10 @@ public class GroceryTestPage {
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "chromedriver/chromedriver-win64/chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        //options.setHeadless(true);
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.flipkart.com/");
     }
