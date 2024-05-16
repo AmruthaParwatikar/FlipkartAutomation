bca4a71665758b2bd33363643a5045cf4303a873
AmruthaParwatikar <93421756+AmruthaParwatikar@users.noreply.github.com>
2024-05-16 12:29:06 +0530

    CartTestPage.java

--- src/test/java/tests/CartTestPage.java
+++ src/test/java/tests/CartTestPage.java
@@ -20,15 +20,16 @@ public class CartTestPage {
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "chromedriver/chromedriver-win64/chromedriver.exe");
-        
+    
         ChromeOptions options = new ChromeOptions();
         //options.setHeadless(true);
         options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
 
         driver.get("https://www.flipkart.com/apple-iphone-15-black-128-gb/p/itm6ac6485515ae4?pid=MOBGTAGPTB3VS24W&lid=LSTMOBGTAGPTB3VS24WVZNSC6&marketplace=FLIPKART&q=iphone&store=tyy%2F4io&spotlightTagId=BestsellerId_tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=3ed6b2e2-c61c-4602-81ae-a91d00e87cfe.MOBGTAGPTB3VS24W.SEARCH&ppt=sp&ppn=sp&ssid=ylyid41yjk0000001715672147721&qH=0b3f45b266a97d70");
         cartPage = new CartPage(driver);
-    }
+}
+
 
     @Test
     public void testAddToCart() throws InterruptedException{
