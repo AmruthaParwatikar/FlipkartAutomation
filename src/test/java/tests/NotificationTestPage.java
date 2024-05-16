6fae26210068870290c9a374fba173cf5458b2d0
AmruthaParwatikar <93421756+AmruthaParwatikar@users.noreply.github.com>
2024-05-16 15:31:54 +0530

    NotificationTestPage.java

--- src/test/java/tests/NotificationTestPage.java
+++ src/test/java/tests/NotificationTestPage.java
@@ -3,6 +3,7 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -22,7 +23,10 @@ public class NotificationTestPage {
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "chromedriver/chromedriver-win64/chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        //options.setHeadless(true);
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.get("https://www.flipkart.com");
         notificationPage = new NotificationPage(driver);
     }
